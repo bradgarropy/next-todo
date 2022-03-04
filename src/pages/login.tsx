8a632d4412c54552472ac0013864365c0bfc54c5
Brad Garropy <bgarropy@atlassian.com>
2022-03-04 11:54:22 -0600

    only redirect when user is present

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,33 @@
 import SEO from "@bradgarropy/next-seo"
+import {useUser} from "@supabase/supabase-auth-helpers/react"
 import Layout from "components/Layout"
 import {useRouter} from "next/router"
-import {FC, FormEventHandler, useState} from "react"
+import {FC, FormEventHandler, useEffect, useState} from "react"
 import {supabase} from "utils/supabase"
 
 type LoginPageProps = unknown
 
 const LoginPage: FC<LoginPageProps> = () => {
+    const {user} = useUser()
     const router = useRouter()
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    const handleSignup: FormEventHandler<HTMLFormElement> = async event => {
+    const handleSignin: FormEventHandler<HTMLFormElement> = async event => {
         event.preventDefault()
 
         await supabase.auth.signIn({
             email,
             password,
         })
-
-        router.push("/todos")
     }
+
+    useEffect(() => {
+        if (user) {
+            router.push("/todos")
+        }
+    }, [user, router])
 
     return (
         <Layout>
@@ -29,7 +35,7 @@ const LoginPage: FC<LoginPageProps> = () => {
 
             <h1>login</h1>
 
-            <form onSubmit={handleSignup}>
+            <form onSubmit={handleSignin}>
                 <label htmlFor="email">Email</label>
                 <input
                     id="email"
