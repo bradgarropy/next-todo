1d9367518358378719558f0a83f9df8738dacd48
Brad Garropy <bradgarropy@gmail.com>
2022-03-06 14:26:37 -0600

    signup check email.

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,5 @@
 import SEO from "@bradgarropy/next-seo"
+import {User} from "@supabase/supabase-js"
 import Layout from "components/Layout"
 import {FC, FormEventHandler, useState} from "react"
 import {supabase} from "utils/supabase"
@@ -8,14 +9,17 @@ type SignupPageProps = unknown
 const SignupPage: FC<SignupPageProps> = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [user, setUser] = useState<User>(null)
 
     const handleSignup: FormEventHandler<HTMLFormElement> = async event => {
         event.preventDefault()
 
-        await supabase.auth.signUp({
+        const {user} = await supabase.auth.signUp({
             email,
             password,
         })
+
+        setUser(user)
     }
 
     return (
@@ -24,24 +28,28 @@ const SignupPage: FC<SignupPageProps> = () => {
 
             <h1>signup</h1>
 
-            <form onSubmit={handleSignup}>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    type="email"
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                />
+            {user ? (
+                <p>{`Go check your email, ${user.email}.`}</p>
+            ) : (
+                <form onSubmit={handleSignup}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
 
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                />
-                <button type="submit">signup</button>
-            </form>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                    <button type="submit">signup</button>
+                </form>
+            )}
         </Layout>
     )
 }
