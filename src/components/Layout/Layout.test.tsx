46175465171595e4b77fb1e1675723531d461b60
Brad Garropy <bgarropy@atlassian.com>
2022-03-08 12:29:08 -0600

    layout and navigation tests.

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,12 +1,30 @@
 import {render, screen} from "@testing-library/react"
 import Layout from "components/Layout"
+import {supabase} from "utils/supabase"
+
+jest.mock("utils/supabase", () => {
+    return {
+        supabase: {
+            auth: {
+                user: jest.fn(),
+            },
+        },
+    }
+})
+
+const mockAuthUser = jest.mocked(supabase.auth.user)
 
 test("shows layout", () => {
-    // render(
-    //     <Layout>
-    //         <p>Testing</p>
-    //     </Layout>,
-    // )
-    // expect(screen.getByText("Home"))
-    // expect(screen.getByText("Testing"))
+    mockAuthUser.mockReturnValue(null)
+
+    render(
+        <Layout>
+            <p>testing</p>
+        </Layout>,
+    )
+
+    expect(screen.getByText("home"))
+    expect(screen.getByText("signup"))
+    expect(screen.getByText("login"))
+    expect(screen.getByText("testing"))
 })
--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,7 +1,48 @@
 import {render, screen} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Navigation from "components/Navigation"
+import {mockUser} from "test-utils/mocks"
+import {supabase} from "utils/supabase"
 
-test("shows navigation", () => {
-    // render(<Navigation />)
-    // expect(screen.getByText("Home"))
+jest.mock("utils/supabase", () => {
+    return {
+        supabase: {
+            auth: {
+                user: jest.fn(),
+                signOut: jest.fn(),
+            },
+        },
+    }
+})
+
+const mockAuthUser = jest.mocked(supabase.auth.user)
+const mockAuthSignOut = jest.mocked(supabase.auth.signOut)
+
+test("shows unauthenticated navigation", () => {
+    mockAuthUser.mockReturnValue(null)
+
+    render(<Navigation />)
+
+    expect(screen.getByText("home"))
+    expect(screen.getByText("signup"))
+    expect(screen.getByText("login"))
+})
+
+test("shows authenticated navigation", () => {
+    mockAuthUser.mockReturnValue(mockUser)
+
+    render(<Navigation />)
+
+    expect(screen.getByText("home"))
+    expect(screen.getByText("todos"))
+    expect(screen.getByText("logout"))
+})
+
+test("logs out", () => {
+    mockAuthUser.mockReturnValue(mockUser)
+
+    render(<Navigation />)
+
+    userEvent.click(screen.getByText("logout"))
+    expect(mockAuthSignOut).toHaveBeenCalledTimes(1)
 })
